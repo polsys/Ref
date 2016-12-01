a39fc451860ad698e5824e07d4a9ffc7c2e06a0e
Petri Laarne <polsys@users.noreply.github.com>
2016-12-01 10:32:34 +0200

    Show volume number along the entry title

---
+++ Ref.Windows/Xaml/EntryTitleConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+using Polsys.Ref.ViewModels;
+
+namespace Polsys.Ref.Xaml
+{
+    /// <summary>
+    /// A value converter that returns either the title, augmented with possible volume information,
+    /// or an "Untitled" value.
+    /// </summary>
+    public class EntryTitleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is EntryViewModelBase)
+            {
+                var entry = value as EntryViewModelBase;
+
+                // If the entry is a book with a specified volume
+                if (entry is BookViewModel)
+                {
+                    var book = entry as BookViewModel;
+                    if (!string.IsNullOrWhiteSpace(book.Volume))
+                    {
+                        return book.Title + " (" + book.Volume + ")";
+                    }
+                }
+
+                // Else just return the entry title
+                return entry.Title;
+            }
+
+            // If the value is not an entry, signal an error
+            return "???";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

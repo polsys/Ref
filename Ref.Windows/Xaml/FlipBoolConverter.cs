71fa96911b0af18b92e6b0fa83374b5fb5ecf280
Petri Laarne <polsys@users.noreply.github.com>
2017-02-24 14:59:35 +0200

    Use IsEnabled instead of IsReadOnly on thesis kind combo box

---
+++ Ref.Windows/Xaml/FlipBoolConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Polsys.Ref.Xaml
+{
+    /// <summary>
+    /// Put in a bool, get its inverse back.
+    /// Put in something else, get an exception.
+    /// </summary>
+    /// <remarks>
+    /// Because IsReadOnly does not prevent combo box selection, which can be prevented with IsEnabled,
+    /// which is of course the opposite of IsReadOnly.
+    /// </remarks>
+    public class FlipBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
